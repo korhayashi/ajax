ecede88b224e6a361f09cc34da35afa83e265d45
kaori hayashi <suzu_33@hotmail.com>
2020-03-21 21:18:54 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
+
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_061918) do
+ActiveRecord::Schema.define(version: 2020_03_21_112623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_02_16_061918) do
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "blog_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -33,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_02_16_061918) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "comments", "blogs"
 end
